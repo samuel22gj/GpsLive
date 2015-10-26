89f9180bb7f1b00c18b4b3a09cc2681b54885ed9
samuel22gj <samuel22gj@gmail.com>
2015-10-26 19:49:11 +0800

    Show empty hint, when can't find any satellite

--- app/src/main/java/com/otter/gpslive/SatelliteFragment.java
+++ app/src/main/java/com/otter/gpslive/SatelliteFragment.java
@@ -8,17 +8,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.otter.gpslive.eventbus.BusProvider;
 import com.otter.gpslive.eventbus.SatellitesChangedEvent;
 import com.squareup.otto.Subscribe;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SatelliteFragment extends Fragment {
     private static final String TAG = SatelliteFragment.class.getSimpleName();
 
+    private RecyclerView satellite_recycler;
     private SatelliteAdapter mSatelliteAdapter;
+
+    private TextView satellite_empty_txt;
 
     public static SatelliteFragment newInstance() {
         return new SatelliteFragment();
@@ -42,8 +47,10 @@ public class SatelliteFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_satellite, container, false);
 
+        satellite_recycler = (RecyclerView) root.findViewById(R.id.satellite_recycler);
+        satellite_empty_txt = (TextView) root.findViewById(R.id.satellite_empty_txt);
+
         // Initial RecyclerView.
-        RecyclerView satellite_recycler = (RecyclerView) root.findViewById(R.id.satellite_recycler);
         satellite_recycler.setHasFixedSize(true);
         satellite_recycler.setLayoutManager(
                 new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
@@ -70,6 +77,15 @@ public class SatelliteFragment extends Fragment {
 
     @Subscribe
     public void onSatellitesChanged(SatellitesChangedEvent event) {
-        mSatelliteAdapter.swap(event.getSatellites());
+        List<GpsSatellite> satellites = event.getSatellites();
+
+        if (satellites == null || satellites.size() == 0) {
+            satellite_empty_txt.setVisibility(View.VISIBLE);
+            satellite_recycler.setVisibility(View.GONE);
+        } else {
+            satellite_empty_txt.setVisibility(View.GONE);
+            satellite_recycler.setVisibility(View.VISIBLE);
+            mSatelliteAdapter.swap(event.getSatellites());
+        }
     }
 }
